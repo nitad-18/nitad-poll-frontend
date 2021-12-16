be37ea9dac66c8eb2f266dbdceca6699885a38aa
Arm <69001162+arskiir@users.noreply.github.com>
2021-12-16 11:19:08 +0700

    ♻️ renamed a variable

--- src/lib/PollCard/PollOption.svelte
+++ src/lib/PollCard/PollOption.svelte
@@ -8,7 +8,7 @@
   const optionColors = ['bg-blue-300', 'bg-green-300', 'bg-red-300']
 
   $: [option, votes] = pollOption
-  $: width = (votes / totalVotes) * 100 + '%'
+  $: bgWidth = (votes / totalVotes) * 100 + '%'
   $: bgColor = optionColors[index % 3]
 </script>
 
@@ -16,7 +16,7 @@
   class="bg-slate-500 rounded relative h-14 overflow-hidden
         cursor-pointer"
 >
-  <div style="width: {width};" class="absolute h-full {bgColor}" />
+  <div style="width: {bgWidth};" class="absolute h-full {bgColor}" />
   <span
     class="px-4 flex items-center absolute h-full font-semibold
     text-slate-900">{option} ({votes})</span
