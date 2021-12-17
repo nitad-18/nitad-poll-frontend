d7e3775aaafdd41a89ce2c361c5051cbbc49a75a
Arm <69001162+arskiir@users.noreply.github.com>
2021-12-17 11:47:51 +0700

    ⚰️ removed old poll store code

--- src/stores/PollStore.ts
+++ src/stores/PollStore.ts
@@ -1,33 +1,6 @@
 import { writable } from 'svelte/store'
 import type { SortMode, PollDetail } from '$lib/types'
 
-// const pollStore = writable<PollDetail[]>([
-//   {
-//     id: '1',
-//     question: 'Python or JavaScript?',
-//     options: [
-//       ['Python', 15],
-//       ['JavaScript', 9],
-//     ],
-//     votes: 24,
-//     open: true,
-//     createdAt: new Date(),
-//   },
-//   {
-//     id: '2',
-//     question: 'Comp Sys Arch or HW Syn Lab?',
-//     options: [
-//       ['Comp Sys Arch', 25],
-//       ['HW Syn Lab', 9],
-//     ],
-//     votes: 34,
-//     open: false,
-//     createdAt: new Date(),
-//   },
-// ])
-
-// export default pollStore
-
 /**
  * a custom store is just a function that returns at least a `subscribe` function
  */
