76e03df86363660bb770bd43a9ed64c4c3139202
Arm <69001162+arskiir@users.noreply.github.com>
2021-12-19 15:50:05 +0700

    🏷️ updated all types to mirror those of the main branch

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,12 +1,19 @@
-export type PollOption = [string /* option */, number /* votes on this option*/]
+export type PollOption = [
+  number /* id */,
+  string /* option */,
+  number /* votes on this option*/
+]
 
 export type PollDetail = {
   id: number
   question: string
   options: PollOption[]
   votes: number // total votes on this poll
-  open: boolean // true means the poll is currently open
-  createdAt: Date
+  isClose: boolean // true means the poll is currently open
+  author: User // author of poll
+  createdDate: Date
+  closedDate?: Date
+  users: User[] // users who voted on this poll
 }
 
 export type SortMode = 'latest' | 'popularity'
@@ -14,4 +21,12 @@ export type SortMode = 'latest' | 'popularity'
 export type PagePathDetail = {
   path: string
   name: string
+}
+
+export type LoginRegisterMode = 'login' | 'register'
+
+export type User = {
+  id: number
+  username: string
+  displayName: string
 }
