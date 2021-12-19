ad118e9992d33c03612888236a603ca483039f9f
Arm <69001162+arskiir@users.noreply.github.com>
2021-12-19 17:37:13 +0700

    ♻️ refactored axios create and get polls

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -33,68 +33,114 @@ const checkMe = async (): Promise<User | null> => {
   return null
 }
 
-const getPolls = async (): Promise<PollDetail[]> => {
-  const response = await instance.get('polls').catch(() => ({
-    data: [],
-  }))
-  let polls = []
+const getPolls = async (): Promise<void> => {
+  // the type of data that the server returns
+  type GetPollsResponseData = {
+    id: number
+    question: string
+    isClose: boolean
+    closedDate: string | null
+    createdDate: string
+    author: User
+    options: {
+      id: number
+      topic: string
+      votes: number
+    }[]
+    users: User[]
+  }[]
 
-  try {
-    polls = response.data
-    pollStore.set(
-      polls.map((poll) => {
-        let votes = 0
-        const options = []
+  const response = await instance.get<GetPollsResponseData>('polls').catch<{
+    data: GetPollsResponseData
+  }>(() => ({ data: [] }))
+  const { data: polls } = response
 
-        for (const option of poll.options) {
-          options.push(Object.values(option))
-          votes += option.votes
-        }
+  // if something went wrong with the request, data is set to [] with the catch
+  if (!polls) return
 
-        return {
-          id: poll.id,
-          question: poll.question,
-          options,
-          votes,
-          isClose: poll.isClose,
-          author: poll.author,
-          createdDate: new Date(poll.createdDate),
-          closedDate: poll.closedDate ? new Date(poll.closedDate) : null,
-          users: poll.users,
-        }
-      })
-    )
-  } catch (err) {
-    return []
-  }
+  // polls from the server are stored differently than the polls in the store
+  const transformedPolls = polls.map((poll) => {
+    let votes = 0
+    const options: PollOption[] = []
+
+    for (const { id, topic, votes: voteOnThisOption } of poll.options) {
+      options.push([id, topic, voteOnThisOption])
+      votes += voteOnThisOption
+    }
+
+    return {
+      id: poll.id,
+      question: poll.question,
+      options,
+      votes,
+      isClose: poll.isClose,
+      author: poll.author,
+      createdDate: new Date(poll.createdDate),
+      closedDate: poll.closedDate ? new Date(poll.closedDate) : null,
+      users: poll.users,
+    }
+  })
+  pollStore.set(transformedPolls)
 }
 
+/**
+ * There are 2 steps to creating a poll:
+ * 1. request to create a poll
+ * 2. for each option, request to insert the option to the poll
+ * @param question the question of the poll
+ * @param options options of the poll
+ * @returns PollDetail if the poll was created successfully, null otherwise
+ */
 const createPoll = async (
   question: string,
   options: [string][]
-): Promise<PollDetail> => {
-  const pollRes = await instance
-    .post('polls', { question })
-    .catch(() => ({ data: [] }))
-  let allOptions: PollOption[]
-  if (pollRes.data) {
-    allOptions = []
-    for (const [option] of options) {
-      if (!option) continue
-      const optionRes = await instance.post('options', {
-        topic: option,
-        pollId: pollRes.data.id,
-      })
-      const { id, topic, votes } = optionRes.data
-      allOptions.push([id, topic, votes])
-    }
+): Promise<PollDetail | null> => {
+  type PostPollsResponseData = {
+    author: User
+    closedDate: string | null
+    createdDate: string
+    id: number
+    isClose: boolean
+    question: string
+    updatedDate: string
   }
+  type PostOptionsResponseData = {
+    id: number
+    topic: string
+    votes: number
+    createdDate: string
+    updatedDate: string
+  }
+
+  // step 1: create the poll
+  const postPollsRes = await instance
+    .post<PostPollsResponseData>('polls', { question })
+    .catch<{ data: null }>(() => ({ data: null }))
+
+  if (!postPollsRes.data) return null
+
+  const {
+    data: { id: pollId, author, createdDate, isClose },
+  } = postPollsRes
+
+  const allOptions: PollOption[] = []
+  // step 2: insert the options to the poll
+  for (const [option] of options) {
+    if (!option) continue
+    const optionRes = await instance.post<PostOptionsResponseData>('options', {
+      topic: option,
+      pollId,
+    })
+    const { id: optionId, topic, votes } = optionRes.data
+    allOptions.push([optionId, topic, votes])
+  }
+
   return {
-    id: pollRes.data.id,
-    author: pollRes.data.author,
-    createdDate: new Date(pollRes.data.createdDate),
-    isClose: pollRes.data.isClose,
-    question: pollRes.data.question,
+    id: pollId,
+    author,
+    createdDate: new Date(createdDate),
+    isClose,
+    question,
     options: allOptions,
     votes: 0,
     users: [],
